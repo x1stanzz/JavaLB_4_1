d673c387cf9d9b52ca18b72d6d507555dcc31db1
x1stanzz <vova28032003@gmail.com>
2023-10-06 17:55:19 +0300

    Created test cases

---
+++ src/test/java/testPalindrome.java
@@ -0,0 +1,21 @@
+import org.example.Palindrome;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class testPalindrome {
+    @Test
+    public void testIsPalindrome(){
+        Assertions.assertTrue(Palindrome.isPalindrome("level"));
+        Assertions.assertTrue(Palindrome.isPalindrome("Rotator"));
+        Assertions.assertTrue(Palindrome.isPalindrome("UFO tofu"));
+        Assertions.assertTrue(Palindrome.isPalindrome("No lemon, no melon"));
+        Assertions.assertTrue(Palindrome.isPalindrome("Step on no pets?"));
+        Assertions.assertTrue(Palindrome.isPalindrome("Yo, Banana boy!"));
+        Assertions.assertTrue(Palindrome.isPalindrome("            "));
+    }
+    @Test
+    public void testNotPalindrome(){
+        Assertions.assertFalse(Palindrome.isPalindrome("123432"));
+        Assertions.assertFalse(Palindrome.isPalindrome("Not a palindrome!"));
+        Assertions.assertFalse(Palindrome.isPalindrome("rkrkrkrk"));
+    }
+}
